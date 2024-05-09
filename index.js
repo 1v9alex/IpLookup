e068d080129ceb6f9b4239bb9f2a7364ff95427f
1v9alex <alexgasee03@gmail.com>
2024-05-08 22:26:17 -0400

    write documentation

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const ipRoutes = require('./routes/ipRoutes');
-
+const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/ip', ipRoutes);
+
+app.get('/api/iplookup/documentation', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'documentation.html'));
+});
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message });
 });
 
-// Start the server
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
